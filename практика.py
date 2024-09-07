059eaf62f0ff58761b3578bdd0ae7b13535f9ca1
Orest99 <orest.yakobchuk@gmail.com>
2024-09-07 13:08:02 +0300

    Add files via upload

---
+++ практика.py
@@ -0,0 +1,16 @@
+text = "Hello World"
+f_name = "Orest"
+l_name = "Yakobchuk"
+age = 17
+
+print("Text:", text)
+print("Typ:", type(text))
+
+print("Text", f_name)
+print("Type:", type(f_name))
+
+print("Text", l_name)
+print("Typ:", type(l_name))
+
+print("Age:", age)
+print("Typ:", type(age))
