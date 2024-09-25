d2f26c5b816bbaf4429da8b8999ec14a515c676f
Orest99 <orest.yakobchuk@gmail.com>
2024-09-25 18:10:20 +0300

    Update в процесі.py

--- в процесі.py
+++ в процесі.py
@@ -1,24 +1,25 @@
 list1 = [1,2,3,4,5,6,3,4,5,7,6,5,4,3,4,5,4,3, 'Привіт', 'анаконда']
 
 def getList(list1):
-    list2 = []
-    for item in list1:
-        if item not in list2:
-            list2.append(item)
-    return list2
-
+    dubl = set(list1)
+    poslidovnist = list(dubl)
+    return poslidovnist
+    
 def getSortList():
-    int chisla=[]
-    str text=[]
-     for item in list2:
-    chisla.append(item)
-    text.append(text)
+     chisla=[]
+     text=[]
+for item in poslidovnist:
+    if type(item) == int:
+        chisla.append(item)
+    elif type(item) == str:
+        text.append(item)
+
     chisla.sort()
     text.sort()
-    
     sorted_list = chisla + text
     return sorted_list
-res = getList(list1)
-sorted_list = getSortList(res)
-print("Список без повторень:", res)
+poslidovnist = getList(list1)#без повторів
+sorted_list = getSortList(poslidovnist)
+
+print("Список без повторень:", poslidovnist)
 print("Відсортований список:", sorted_list)
